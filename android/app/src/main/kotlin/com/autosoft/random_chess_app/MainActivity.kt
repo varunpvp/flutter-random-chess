dca218b965b8016e9aa614a08159229d7d9e58b0
varunpvp <varunpvp@gmail.com>
2021-02-09 12:10:29 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/autosoft/random_chess_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.autosoft.random_chess_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart';
+
+import 'utils.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Random Chess"),
+      ),
+      body: Center(
+        child: Chessboard(
+          fen: _fen,
+          size: size.width,
+          onMove: (move) {
+            final nextFen = makeMove(_fen, {
+              'from': move.from,
+              'to': move.to,
+              'promotion': 'q',
+            });
+
+            if (nextFen != null) {
+              setState(() {
+                _fen = nextFen;
+              });
+
+              Future.delayed(Duration(milliseconds: 300)).then((_) {
+                final nextMove = getRandomMove(_fen);
+                setState(() {
+                  _fen = makeMove(_fen, nextMove);
+                });
+              });
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
