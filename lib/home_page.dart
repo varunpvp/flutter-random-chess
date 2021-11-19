a10f7642f5e78e9154680cd45c3fafd8e1167384
Varun Pujari <varunpvp@gmail.com>
2021-11-19 12:02:18 +0530

    use promotion

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart'
-    as cb;
+import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart';
+import 'package:flutter_stateless_chessboard/types.dart';
 
 import 'utils.dart';
 
@@ -18,21 +18,62 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final viewport = MediaQuery.of(context).size;
     final size = min(viewport.height, viewport.width);
+    final navigator = Navigator.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Random Chess"),
       ),
       body: Center(
-        child: cb.Chessboard(
+        child: Chessboard(
           fen: _fen,
           size: size,
-          orientation: cb.Color.WHITE,
+          orientation: ChessColor.WHITE,
+          onPromote: () {
+            return showDialog<PieceType>(
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  title: Text('Promotion'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        title: Text("Queen"),
+                        onTap: () {
+                          navigator.pop(PieceType.QUEEN);
+                        },
+                      ),
+                      ListTile(
+                        title: Text("Rook"),
+                        onTap: () {
+                          navigator.pop(PieceType.ROOK);
+                        },
+                      ),
+                      ListTile(
+                        title: Text("Bishop"),
+                        onTap: () {
+                          navigator.pop(PieceType.BISHOP);
+                        },
+                      ),
+                      ListTile(
+                        title: Text("Knight"),
+                        onTap: () {
+                          navigator.pop(PieceType.KNIGHT);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
           onMove: (move) {
             final nextFen = makeMove(_fen, {
               'from': move.from,
               'to': move.to,
-              'promotion': 'q',
+              'promotion':
+                  move.promotion.map((t) => t.toString()).getOrElse(() => "q"),
             });
 
             if (nextFen != null) {
